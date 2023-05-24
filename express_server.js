3cb8a0d00be1442dcb8122749d612445d5fea3e2
JacquelineMG <j.gilchrist@outlook.com>
2023-05-24 13:39:11 -0700

    reworking url table and link

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs");
 
 
 const urlDatabase = {
+  // ids === short URL codes
+  // urls[ids] === long urls
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -15,6 +17,11 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {
