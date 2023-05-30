8518f4cc6848b984ee9ad9363b3ba8376d0a5314
JacquelineMG <j.gilchrist@outlook.com>
2023-05-30 12:50:15 -0700

    add registration capability and update login

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+// Project Description: A full stack web app built with Node and Express that allows users to shorten long URLs
+
 // Server Setup and Middleware:
 const express = require("express");
 const app = express();
@@ -10,20 +12,18 @@ const morgan = require("morgan");
 app.use(morgan('dev'));
 
 const crypto = require("crypto");
-const { url } = require("inspector");
-
+ 
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-// Helper Functions:
-// Random code generator:
-const generateRandomString = function() {
-  const id = crypto.randomBytes(3).toString('hex');
-  return id
-};
+
+//Listener:
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 
-// Database:
+// ID Database:
 const urlDatabase = {
   // id === short URL codes
   // urls[id] === long urls
@@ -31,79 +31,131 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//CREATE: show create new URL
+// User Database:
+const users = {
+  abc123: {
+    id: "abc123",
+    email: "a@a.com",
+    password: "123"
+  },
+  def456: {
+    id: "def456",
+    email: "d@d.com",
+    password: "456"
+  }
+};
 
-//SAVE: submit new URL
+// Helper Functions:
 
-//LOGIN 
-app.post("/login", (req, res) => {
-  const cookie = req.body.username;
-  res.cookie("username", cookie);
-  res.redirect("/urls");
+// Random code generator:
+
+const generateRandomString = function() {
+  const id = crypto.randomBytes(3).toString('hex');
+  return id;
+};
+
+// Email lookup:
+
+const findUserByEmail = function (newEmail) {
+  for (const user in users) {
+    if (users[user].email === newEmail) {
+      return users[user];
+    }
+  } return null;
+};
+
+
+
+
+////////////////////////
+////////  GETS  ////////
+////////////////////////
+
+
+// Registration page:
+
+app.get("/register", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const user = users[userID];
+
+  const templateVars = {
+    user
+  }
+  res.render("urls_register", templateVars);
 });
 
-// LOGOUT
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
+
+// Login page:
+
+app.get("/login", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const user = users[userID];
+
+  const templateVars = {
+    user
+  };
+res.render("urls_login",  templateVars)
 });
 
 
-//INDEX: display all URLS
+// Display all URLS page:
+
 app.get("/urls", (req, res) => {
-  const templateVars = { 
+  const userID = req.cookies["user_id"];
+  const user = users[userID];
+
+  const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
- };
+    user
+  };
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  const newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = req.body.longURL;
-  res.redirect(`/urls/${newShortURL}`);
-});
 
-//SAVE: Submit new URL
+// Create new URL page:
+
 app.get("/urls/new", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const user = users[userID];
+
   const templateVars = {
-    username: req.cookies["username"]
+    user
   };
   res.render("urls_new", templateVars);
 });
+
+
+// Individual shortened URL page:
+
+app.get("/urls/:id", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const user = users[userID];
+
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    user
+  };
+  res.render("urls_show", templateVars);
+});
+
+
+// Individual redirect links page:
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"] 
-  };
-  res.render("urls_show", templateVars);
-});
 
-
-//UPDATE: Submit edited URL
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURL
-  res.redirect("/urls");
-});
-
-
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
-});
-
+// Json:
 
 app. get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+
+// Test/temp pages: 
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -115,7 +167,93 @@ app.get("/hello", (req, res) => {
 });
 
 
-//Listener:
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+
+////////////////////////
+////////  POSTS  ///////
+////////////////////////
+
+
+// Registration process:
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email === "" || password === "") {
+    return res.status(400).send("Please fill in all the requested fields.");
+  };
+
+  if (findUserByEmail(email)) {
+    return res.status(400).send("Email is already register. Please login.");
+  };
+
+const userID = generateRandomString();
+
+users[userID] = {
+  id: userID,
+  email,
+  password
+};
+
+res.cookie("user_id", userID);
+res.redirect("/urls");
+});
+
+
+//Login process:
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(400).send("Please fill in all requested fields.");
+  };
+
+  if (!findUserByEmail(email)) {
+    return res.status(403).send("Email not found. Please register.");
+  };
+
+  if (findUserByEmail(email).password !== password) {
+    return res.status(403).send("Wrong password. Please try again.");
+  };
+
+  const userID = findUserByEmail(email).id;
+
+  res.cookie("user_id", userID);
+  res.redirect("/urls");
+  });
+
+
+// Logout process:
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect("/login");
+});
+
+
+// Add new URL process:
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);
+});
+
+
+// Edit URL process:
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  res.redirect("/urls");
+});
+
+
+// Delete URL process:
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
 });
